f72fc2476e11e899b518fed6dabc1b3f7634b523
Emily Aguilar-Pine <102376315+emily-ap@users.noreply.github.com>
2024-10-16 22:32:15 -0500

    Update 1_porechop.sh

--- 1_read_processing/1_porechop.sh
+++ 1_read_processing/1_porechop.sh
@@ -1,1 +1,7 @@
+threads= 
+reads= 
+output=
+verbosity=
+# ^^^ normally I do 1 or 2, this is just how much output do you want to actually see.
 
+porechop -t $threads -i $reads -o $output --verbosity $verbosity
